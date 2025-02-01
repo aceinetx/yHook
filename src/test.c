9aeb36625bad77ca8e211104b1ec9f25d8784090
aceinetx <mouselin82@gmail.com>
2025-02-01 16:17:52 +0300

    more in-depth test

--- src/test.c
+++ src/test.c
@@ -2,19 +2,28 @@
 #include <stdio.h>
 
 yHook_t hook;
+yHook_t hook2;
 
 void orig(int x) { printf("called orig(%d)\n", x); }
 
 void hooked(int x) {
   printf("called hooked(%d)\n", x);
-  x = 123;
   yHookDisable(hook);
   orig(x);
   yHookEnable(hook);
 }
 
+void hooked2(int x) {
+  printf("called hooked2(%d)\n", x);
+  yHookDisable(hook2);
+  orig(x);
+  yHookEnable(hook2);
+}
+
 int main() {
   hook = yHookInstall(orig, hooked);
   yHookEnable(hook);
+  hook2 = yHookInstall(orig, hooked2);
+  yHookEnable(hook2);
   orig(69);
 }
