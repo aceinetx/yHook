15f610e3b01d08577f2777e4478f98c5119ba580
aceinetx <mouselin82@gmail.com>
2025-02-02 12:08:38 +0300

    arm support #2

--- src/yhook.c
+++ src/yhook.c
@@ -3,6 +3,8 @@
  * Copyright (c) aceinet
  * License: GPL-2.0
  */
+#ifndef YHOOK_ARM
+
 #include "yhook.h"
 #include <stdbool.h>
 #include <stdint.h>
@@ -103,3 +105,77 @@ int yHookDisable(yHook_t hook) {
 
   return 0;
 }
+
+#else
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+int yHookProtect(void *address, size_t size, int prot) {
+  long page_size;
+  void *aligned_address;
+  void *end;
+  size_t new_size;
+
+  page_size = sysconf(_SC_PAGESIZE);
+  aligned_address = (void *)((long)address & ~(page_size - 1));
+
+  end = address + size;
+  new_size = end - aligned_address;
+
+  int error = mprotect(aligned_address, new_size, prot);
+  return error;
+}
+
+attribute((naked)) void opcarm() {
+  asm volatile("movw r0, #0x5678\n\t" // Load lower 16 bits of address
+               "movt r0, #0x1234\n\t" // Load upper 16 bits of address
+               "bx   r0\n\t"          // Jump to address in r0
+  );
+}
+
+void __yHookPatch(uint32_t new_addr) {
+  uint8_t *code = (uint8_t *)opcarm;
+
+  prot(opcarm, 8, PROT_WRITE | PROT_EXEC | PROT_READ);
+
+  // Patch the MOVT instruction (lower 16 bits)
+  code[4] = new_addr >> 16 & 0xff;
+  code[5] = new_addr >> 16 >> 8 & 0xf;
+  code[6] = (new_addr >> 16 >> 12 & 0xf) + 0x40;
+
+  // Patch the MOVW instruction (upper 16 bits)
+  code[0] = new_addr & 0xff;
+  code[1] = new_addr >> 8 & 0xf;
+  code[2] = new_addr >> 12 & 0xf;
+
+  // Ensure cache coherence
+  builtin_clear_cache(code, code + 8);
+}
+
+yHook_t yHookInstall(void *from, void *to) {
+  yHook_t hook;
+
+  hook.from = from;
+  hook.to = to;
+
+  memcpy(hook.originalCode, hook.from, JMP_SIZE);
+
+  return hook;
+}
+
+void yHookEnable(yHook_t hook) {
+  prot(hook.from, JMP_SIZE, PROT_WRITE | PROT_EXEC | PROT_READ);
+
+  yHookPatch((uint32_t)hook.to);
+  memcpy(hook.from, opcarm, JMP_SIZE);
+  __builtin___clear_cache(hook.from, hook.from + JMP_SIZE);
+}
+
+void yHookDisable(yHook_t hook) {
+  prot(hook.from, JMP_SIZE, PROT_WRITE | PROT_EXEC | PROT_READ);
+
+  memcpy(hook.from, hook.originalCode, JMP_SIZE);
+  __builtin___clear_cache(hook.from, hook.from + JMP_SIZE);
+}
+#endif
